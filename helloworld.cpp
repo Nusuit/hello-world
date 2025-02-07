b6843c102cd91806f63cdd93dba30539f6db34ec
Nusuit <huykien283@gmail.com>
2025-02-07 16:02:51 +0700

    Write helloworld and introduce myself

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string n; 
+    int m;
+    cout<<"Enter your name: ";
+    cin>>n;
+    cout<<"Enter your age: ";
+    cin>>m;
+    cout<<"My name is "<<n<<". My age is "<<m;
+}
---
+++ helloworld.py
@@ -0,0 +1,3 @@
+a = input("Please enter your name: ")
+b = int(input("Enter your age: "))
+print ("My name is ", a, ". My age is ", b)
