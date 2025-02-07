4837b1a548f327425f6bbaeed607c8269d17b480
Nusuit <huykien283@gmail.com>
2025-02-07 16:06:38 +0700

    change file cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 int main(){
-    string n; 
-    int m;
-    cout<<"Enter your name: ";
-    cin>>n;
-    cout<<"Enter your age: ";
-    cin>>m;
-    cout<<"My name is "<<n<<". My age is "<<m;
+    int m = 5, n= 10;
+    cout<<m+n;
 }
