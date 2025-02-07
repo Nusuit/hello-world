95e8b5ec7dc65d6391fda053d16a7ba0743bb915
Nusuit <huykien283@gmail.com>
2025-02-07 16:10:50 +0700

    update file cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-    cout<<"hello world!!!";
+    string n; 
+    int m;
+    cout<<"Enter your name: ";
+    cin>>n;
+    cout<<"Enter your age: ";
+    cin>>m;
+    cout<<"My name is "<<n<<". My age is "<<m;
 }
